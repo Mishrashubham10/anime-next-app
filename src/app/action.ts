0def8dfc5df73d9ced1d4d094a2943cf00ee2463
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-11-30 19:00:20 +0530

    Create infinite pagination

--- src/app/action.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-export const fetchAnime = async (page: number) => {
-    const res = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
-
-    const data = await res.json()
-
-    return data;
-}
